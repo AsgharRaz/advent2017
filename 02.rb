d698ba0aea553baa736f2f4946a40fb1484fce3a
Matthias Kulpa <matthias.kulpa@akra.de>
2017-12-03 21:07:31 +0100

    feat: add gold star solution

--- 02.rb
+++ 02.rb
@@ -1,4 +1,13 @@
 I = File.read('puzzles/02')
 v = I.split("\n").map { |e| e.split("\t").map(&:to_i) }
-v = v.map { |e| e.max - e.min }
-p v.sum
+p v.map { |e| e.max - e.min }.sum
+w = []
+v.each do |a|
+  arr = a.sort.reverse
+  arr.each do |e|
+    (arr.reject { |n| n == e }).each do |ie|
+      w << e / ie if (e % ie).zero?
+    end
+  end
+end
+p w.sum
