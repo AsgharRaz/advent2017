7375195ce4c215046136888230e13834a5df71ef
Matthias Kulpa <matthias.kulpa@akra.de>
2017-12-03 21:08:37 +0100

    refa: golf

--- 03.rb
+++ 03.rb
@@ -1,8 +1,7 @@
 I = File.read('puzzles/03')
 def lvl(x, c = 8, i = 1)
-  x -= 1 if i == 1
-  return { pos: x, c: c, i: i } if c > x
+  return { p: x, c: c, i: i } if c > x
   lvl(x - c, c + 8, i + 1)
 end
-v = lvl(I.to_i)
-p v[:i] + (v[:c] / 8 - v[:pos] % (v[:c] / 4)).abs
+v = lvl(I.to_i - 1)
+p v[:i] + (v[:c] / 8 - v[:p] % (v[:c] / 4)).abs
