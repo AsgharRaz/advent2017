abe432d7f94886b64472971953f5acb8069533ca
Matthias Kulpa <matthias.kulpa@akra.de>
2017-12-06 18:28:20 +0100

    refa: golf

--- 02.rb
+++ 02.rb
@@ -1,10 +1,8 @@
 i = File.readlines('p/input02')
 v = i.map { |e| e.split("\t").map(&:to_i) }
-
 w = []
 v.each do |a|
-  a = a.sort.reverse
-  a.each do |e|
+  a = a.sort.reverse.each do |e|
     (a.reject { |n| n == e }).each do |r|
       w << e / r if (e % r).zero?
     end
