148b3d04e8199d0ee400e21797dbec7a26597101
Matthias Kulpa <matthias.kulpa@akra.de>
2017-12-08 09:49:01 +0100

    golf

---
+++ 07.rb
@@ -0,0 +1,21 @@
+i = File.read('p/input07').split("\n")
+s = @p = {}
+i.each { |e| @p[e.split[0].to_sym] = { c: (s.split(', ').map(&:to_sym) unless (s = e.gsub(/.*\> /,'')).split[0] == e.split[0]), w: e.gsub(/.*\(/,'').gsub(/\).*/,'').to_i } }
+
+def w(p)
+  return @p[p][:w] unless @p[p][:c]
+  @p[p][:w] + @p[p][:c].map { |e| w(e) }.inject(:+)
+end
+
+def d(p)
+  t = @p[p][:c].map { |e| [e, w(e)] }.to_h
+  b = (f = t.values.uniq).size > 1 ? f.inject(:-) : 0
+  [b, t.key(b < 0 ? f[1] : f[0])]
+end
+
+def u(p)
+  return p if d(p)[0].zero?
+  u(d(p)[1])
+end
+
+p r = (@p.map { |k, v| k }.compact - @p.map { |k, v| v[:c] }.compact.flatten)[0], @p[u(r)][:w] + d(r)[0]
---
+++ 07_readable.rb
@@ -0,0 +1,29 @@
+i = File.read('p/input07').split("\n")
+search = @progs = {}
+i.each do |e|
+  children = search.split(', ').map(&:to_sym) unless (search = e.gsub(/.*\> /,'')).split[0] == e.split[0]
+  @progs[e.split[0].to_sym] = {
+    children: children,
+    weight: e.gsub(/.*\(/,'').gsub(/\).*/,'').to_i
+  }
+end
+
+def weight(prog)
+  return @progs[prog][:weight] unless @progs[prog][:children]
+  @progs[prog][:weight] + @progs[prog][:children].map { |e| weight(e) }.inject(:+)
+end
+
+def branch_info(prog)
+  children_tower = @progs[prog][:children].map { |e| [e, weight(e)] }.to_h
+  diff = (diffs = children_tower.values.uniq).size > 1 ? diffs.inject(:-) : 0
+  [diff, children_tower.key(diff < 0 ? diffs.max : diffs.min)]
+end
+
+def up_the_tree(prog)
+  obj = branch_info(prog)
+  return prog if obj[0].zero?
+  up_the_tree(obj[1])
+end
+
+p root = (@progs.map { |key, value| key }.compact - @progs.map { |key, value| value[:children] }.compact.flatten)[0]
+p @progs[up_the_tree(root)][:weight] + branch_info(root)[0]
