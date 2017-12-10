d63cbf31616e5a6fe0b38760d82dbcba7ba16148
Matthias Kulpa <matthias.kulpa@akra.de>
2017-12-10 10:55:11 +0100

    golf

--- 10.rb
+++ 10.rb
@@ -1,24 +1,27 @@
 j = []
 i = File.read('p/input10').strip.each_byte { |c| j << c }
 j += [17, 31, 73, 47, 23]
-a = *(0..255)
-f = t = s = 0
-64.times do
-  j.each do |l|
-    t = (f + l - (l.zero? ? 0 : 1)) % a.size
-    if f <= t
-      a[f..t] = a[f..t].reverse
-    else
-      z = a.size - 1
-      o = (a[f..z] + a[0..t]).reverse
-      a[f..z] = o[0..z - f]
-      a[0..t] = o[a.size - f..o.size - 1]
+v = *(0..255)
+def k(j, a, x = 64)
+  f = t = s = 0
+  x.times do
+    j.each do |l|
+      n = 0
+      while 2 * n < l - 1
+        m = a[v = (f + n) % a.size]
+        a[v] = a[t = (f + l - 1 - n) % a.size]
+        a[t] = m
+        n += 1
+      end
+      f = (f + l + s) % a.size
+      s += 1
     end
-    f = (f + l + s) % a.size
-    s += 1
-    p a[0] * a[1] if s == i.size - 1
   end
+  a
 end
+b = k(i.split(',').map(&:to_i), v.clone, 1)
+p b[0] * b[1]
+c = k(j, v)
 h = []
-a.each_slice(16) { |e| h << e.inject(:^).to_s(16) }
+c.each_slice(16) { |e| h << e.inject(:^).to_s(16) }
 p h.join
