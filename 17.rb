3098e43d46c2b03dba3ab46ac5a70ebe150c6202
Matthias Kulpa <matthias.kulpa@akra.de>
2017-12-19 19:10:53 +0100

    golf

--- 17.rb
+++ 17.rb
@@ -1,16 +1,11 @@
-steps = File.read('p/input17').to_i
-arr = [0]
-pos = incr = fvoi = 0
-voi = nil
-
-while incr < 50_000_000
-  incr += 1
-  pos = ((pos + steps) % incr) + 1
-  arr.insert(pos, incr) if incr <= 2017 || pos == 1
-  fvoi = arr[(pos + 1) % incr] if incr == 2017
-  voi = arr[1] if pos == 1
-  print "#{fvoi} | #{voi} (#{incr})\r"
+s = File.read('p/input17').to_i
+a = [0]
+o = i = f = v = 0
+while i < 50_000_000
+  i += 1
+  o = ((o + s) % i) + 1
+  a.insert(o, i) if i <= 2017 || o == 1
+  f = a[(o + 1) % i] if i == 2017
+  v = a[1] if o == 1
 end
-
-
-  p "#{fvoi} | #{voi} (#{incr})"
+p "#{f}, #{v}"
